f7440a2bd7bb20cd4167a72356f3c521e04ed236
amadrafi <ahmadrafi0814@gmail.com>
2024-10-01 15:35:29 +0100

    upadate on chroma db

--- preprocess.py
+++ preprocess.py
@@ -44,17 +44,6 @@ def split_text(documents: list[Document]):
 
     return chunks
 
-# def save_to_chroma(chunks: list[Document]):
-#     # Clear out the database first.
-#     if os.path.exists(CHROMA_PATH):
-#         shutil.rmtree(CHROMA_PATH)
-
-#     # Create a new DB from the documents.
-#     db = Chroma.from_documents(
-#         chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
-#     )
-#     db.persist()
-#     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
 
 def save_to_chroma(chunks: list[Document]):
     # Clear out the database first.
